787fa8b5ca7f53692f2eb6103a3c286eea4a6f7c
Harlock <iscprimerocesar@gmail.com>
2018-05-31 12:06:34 -0500

    revision

--- HP_M/AppData/Model/conexion.php
+++ HP_M/AppData/Model/conexion.php
@@ -5,7 +5,7 @@ namespace AppData\Model;
 
 class conexion
 {
-    private $datos=array("server"=>"localhost","user"=>"root", "password"=>"", "db"=>"hotel");
+    private $datos=array("server"=>"localhost","user"=>"root", "password"=>"", "db"=>"test_hotel");
     private $conexion;
 
     function __construct()
