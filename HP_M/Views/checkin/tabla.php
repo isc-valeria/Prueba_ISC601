bed2ffb72ad1b0c1f8e2dfb219137c2422c5ba1d
Luigigi94 <el.luige94@gmail.com>
2018-05-31 11:06:06 -0500

    new

--- HP_M/Views/checkin/tabla.php
+++ HP_M/Views/checkin/tabla.php
@@ -9,6 +9,14 @@ while($row=mysqli_fetch_array($datos1))
                         <td>{$row[2]}</td>
                         <td>{$row[3]}</td>
                         <td>{$row[4]}</td>
+                        <td>{$row[5]}</td>
+                        <td>{$row[6]}</td>
+                        <td>{$row[7]}</td>
+                        <td>{$row[8]}</td>
+                        <td>{$row[9]}</td>
+                        <td>{$row[10]}</td>
+                        <td>{$row[11]}</td>
+                        <td>{$row[12]}</td>
                         <td><a class=\"btn-flat modal-trigger icon-cross red-text\" href=\"#modal_eliminar\"></td>
                         <td><a class=\"btn-flat modal-trigger  icon-pencil blue-text\" href=\"#modal_editar\"></a></td>
                     </tr>";
