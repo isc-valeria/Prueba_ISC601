022d51886d429afef65b83f80ff61aa90040b7ec
Maricela Policarpo <isc_policarpo.m@tesvb.edu.mx>
2018-06-01 17:53:11 -0500

    controllers

--- HP_M/AppData/Controller/registro_prendasController.php
+++ HP_M/AppData/Controller/registro_prendasController.php
@@ -11,11 +11,16 @@ namespace AppData\Controller;
 
 class registro_prendasController
 {
-    private $registro_prendas;
+    private $registro_prendas , $habitaciones, $tipo_ropa, $estado_prenda;
 
     public function __construct()
     {
         $this->registro_prendas= new \AppData\Model\Registro_prendas();
+        $this->habitaciones= new \AppData\Model\Habitaciones();
+        $this->tipo_ropa= new \AppData\Model\Ropa();
+        $this->estado_prenda= new \AppData\
+
+
     }
 
     public function index()
--- HP_M/Views/registro_prendas/index.php
+++ HP_M/Views/registro_prendas/index.php
@@ -44,7 +44,7 @@
                             </div>
 
                             <div class="input-field col l3">
-                                <label>Fecha de Entrega</label>
+                                <label>Fecha de entrega</label>
                                 <input id="fecha_entre" type="text" class="datepicker">
                             </div>
                         </div>
@@ -59,7 +59,7 @@
                                         <option value="2">Pantalón</option>
                                         <option value="3">Camisa</option>
                                     </select>
-                                    <label for="tiporopa" data-error="incorrecto" data-success="Correcto" >Tipo de Ropa </label>
+                                    <label for="tiporopa" data-error="incorrecto" data-success="Correcto" >Tipo de ropa </label>
                                 </div>
                                 <div class="input-field col s1">
                                     <a class="btn-floating disabled waves-effect waves-light btn modal-trigger" href="#modal_tiporopa" ><i class="icon-plus #00838f cyan darken-3"></i></a>
@@ -147,7 +147,7 @@
                                         <option value="2">300</option>
                                         <option value="3">400</option>
                                     </select>
-                                    <label for="precio" data-error="incorrecto" data-success="Correcto"> Precio</label>
+                                    <label for="precio" data-error="incorrecto" data-success="Correcto"> Precio </label>
                                 </div>
                                 <div class="input-field col s1">
                                     <a class="btn-floating disabled waves-effect waves-light btn modal-trigger" href="#modal_precio" ><i class="icon-plus #00838f cyan darken-3"></i></a>
