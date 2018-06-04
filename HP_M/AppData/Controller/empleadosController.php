7904417fa7bf73240258416833537fb639f66fe6
Christian <isc_victoria.c@tesvb.edu.mx>
2018-06-03 20:23:57 -0500

    christian

--- HP_M/AppData/Controller/empleadosController.php
+++ HP_M/AppData/Controller/empleadosController.php
@@ -16,12 +16,60 @@ class empleadosController
     public function __construct()
     {
         $this->empleados= new \AppData\Model\Empleados();
+        $this->Puestos= new \AppData\Model\Empleados();
     }
 
     public function index()
     {
-        $datos=$this->empleados->getAll();
+        $datos1=$this->empleados->getAll();
+        $datos2=$this->Puestos->getAll();
+        $datos[0]=$datos1;
+        $datos[1]=$datos2;
+
         return $datos;
     }
+    public function crear(){
+        if($_POST)
+        {
+            $this->empleados->set('nombre_emp',$_POST["nombre_emp"]);
+            $this->empleados->set('ap_emp',$_POST["ap_emp"]);
+            $this->empleados->set('am_emp',$_POST["am_emp"]);
+            $this->empleados->set('id_puesto',$_POST["id_puesto"]);
+            $this->empleados->add();
+            $datos1=$this->empleados->getAll();
+            $datos[0]=$datos1;
+            return $datos;
 
+
+        }
+
+    }
+
+    public function eliminar($id)
+    {
+        $this->empleados->delete($id[0]);
+        $datos1=$this->empleados->getAll();
+        $datos[0]=$datos1;
+        return $datos;
+    }
+    public function modificar($id)
+    {
+        $datos=$this->empleados->getOne($id[0]);
+        return $datos;
+    }
+    public function actualizar($id)
+    {
+        if($_POST)
+        {
+            $this->empleados->set("id_empleado",$id[0]);
+            $this->empleados->set('nombre_emp',$_POST["nombre_emp"]);
+            $this->empleados->set('ap_emp',$_POST["ap_emp"]);
+            $this->empleados->set('am_emp',$_POST["am_emp"]);
+            $this->empleados->set('id_puesto',$_POST["id_puesto"]);
+            $this->empleados->update();
+            $datos1=$this->empleados->getAll();
+            $datos[0]=$datos1;
+            return $datos;
+        }
+    }
 }
---
+++ HP_M/Views/empleados/eliminar.php
@@ -0,0 +1,10 @@
+<?php
+$datos=$datos[0];
+while($row=mysqli_fetch_array($datos))
+    echo "<tr><td>{$row[0]}</td>
+                <td>{$row['nombre_emp']}</td>
+                <td>{$row['ap_emp']}</td>
+                <td>{$row['am_emp']}</td><td>{$row['id_puesto']}</td>
+                 <td><a class='btn-flat icon-cross red-text btn_eliminar' href='#!' data-id='{$row['id_empleado']}'></a></td>
+                <td><a class=\"btn-flat modal-trigger  icon-pencil blue-text\" href=\"#modal_editar\"></a></td></tr>";
+?>
