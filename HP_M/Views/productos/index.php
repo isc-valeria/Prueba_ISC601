b83ee931f7f208131f06493eb55023b315382684
Yareli Morales <isc_morales.y@tesvb.edu.mx>
2018-06-05 07:11:58 -0500

    Productos

--- HP_M/Views/productos/index.php
+++ HP_M/Views/productos/index.php
@@ -148,7 +148,7 @@
     </div>
     <div class="modal-footer">
         <a href="#!" id="eliminar_ok" class="modal-close green white-text waves-effect waves-green btn-flat">Aceptar</a>
-        <a href="#!" class="modal-close red white-text waves-effect waves-green btn-flat">Cancelar</a>
+        <a href="#!" id="cancelar" class="modal-close red white-text waves-effect waves-green btn-flat">Cancelar</a>
     </div>
 </div>
 
