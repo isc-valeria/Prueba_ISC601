26dd760e07c105b51bd6cb8dfe9b560b2864baf7
Yareli Morales <isc_morales.y@tesvb.edu.mx>
2018-06-12 13:12:17 -0500

    Template

--- HP_M/Views/Template.php
+++ HP_M/Views/Template.php
@@ -26,15 +26,15 @@ class Template
             <link href="<?php echo URL?>Public/css/style.min.css" type="text/css" rel="stylesheet">
             <link href="<?php echo URL?>Public/css/custom.min.css" type="text/css" rel="stylesheet">
             <link href="<?php echo URL?>Public/css/stilo.css" type="text/css" rel="stylesheet">
-<<<<<<< HEAD
+
             <link rel="stylesheet" href="<?php echo URL?>/Public/style.css">
             <script type="text/javascript" src="<?php echo URL?>/Public/js/plugins/jquery-1.11.2.min.js"></script>
             <script type="text/javascript" src="<?php echo URL?>/Public/js/plugins/pagination.js"></script>
-=======
+
             <link rel="stylesheet" href="<?php echo URL?>Public/style.css">
             <script type="text/javascript" src="<?php echo URL?>Public/js/plugins/jquery-1.11.2.min.js"></script>
             <script type="text/javascript" src="<?php echo URL?>Public/js/plugins/pagination.js"></script>
->>>>>>> 8403e78c041f34411f8c7793817aaf91fea14daf
+
             <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
         <script type="text/javascript" src="<?php echo URL?>/Public/js/plugins/jquery.validate.min.js"></script>
         <script type="text/javascript">
