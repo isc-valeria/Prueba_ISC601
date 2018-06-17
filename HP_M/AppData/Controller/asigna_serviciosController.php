a0d1d5629a944741c66f3676dce99b5882fccc89
SaulMig <isc_miguel.s@tesvb.edu.mx>
2018-06-17 16:44:20 -0500

    saul_Asigna_servicios

--- HP_M/AppData/Controller/asigna_serviciosController.php
+++ HP_M/AppData/Controller/asigna_serviciosController.php
@@ -1,36 +1,43 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Luis
- * Date: 22/05/2018
- * Time: 05:01 AM
+ * User: Sullivan
+ * Date: 17/06/2018
+ * Time: 02:28 PM
  */
+
 
 namespace AppData\Controller;
 
 
 class asigna_serviciosController
 {
-    private $asigna_servicios;
+    private $asignacion_de_servicios,$habitaciones,$servicios,$estado_habitacion,$tipos_habitacion;
 
     public function __construct()
     {
-        $this->asigna_servicios= new \AppData\Model\Asigna_servicios();
+        $this->asignacion_de_servicios= new \AppData\Model\Asigna_servicios();
+        $this->habitaciones=new \AppData\Model\Habitaciones();
+        $this->servicios=new \AppData\Model\Servicios();
+        $this->estado_habitacion=new \AppData\Model\EstadoHabitaciones();
+        $this->tipos_habitacion=new \AppData\Model\Tipo_habitacion();
     }
 
     public function index()
     {
-        $datos1=$this->asigna_servicios->getAll();
-        $datos2=$this->asigna_servicios->getDatos1();
-        $datos3=$this->asigna_servicios->getDatos2();
-        $datos4=$this->asigna_servicios->getDatos3();
+
+        $datos1=$this->asignacion_de_servicios->getAll();
+        $datos2=$this->habitaciones->getAll();
+        $datos3=$this->servicios->getAll();
+        $datos4=$this->estado_habitacion->getAll();
+        $datos5=$this->tipos_habitacion->getAll();
 
         $datos[0]=$datos1;
         $datos[1]=$datos2;
         $datos[2]=$datos3;
         $datos[3]=$datos4;
+        $datos[4]=$datos5;
 
         return $datos;
     }
-
 }
