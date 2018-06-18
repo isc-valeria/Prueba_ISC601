512934ecbfa127d5c08ee5ca2a0f347d52122018
JabneelHer <isc_francisco.j@tesvb.edu.mx>
2018-06-18 14:21:58 -0500

    consulta res

--- HP_M/AppData/Model/conexion.php
+++ HP_M/AppData/Model/conexion.php
@@ -7,7 +7,7 @@ class conexion
 {
 
 
-    private $datos=array("server"=>"localhost","user"=>"root", "password"=>"123456", "db"=>"hotel");
+    private $datos=array("server"=>"localhost","user"=>"root", "password"=>"", "db"=>"hotel");
 
     private $conexion;
 
