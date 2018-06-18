9ecf7cc4329bbbde95d2e00d180d7f186f7ab5fb
MiriamSantanaEspinosa <miriam2397SANTANA@gmail.com>
2018-06-18 13:07:42 -0500

    pdf y grafica

--- HP_M/AppData/Model/Empleados.php
+++ HP_M/AppData/Model/Empleados.php
@@ -70,4 +70,18 @@ class Empleados
 
 
     }
+    function verify()
+    {
+        $sql = "select * from {$this->tabla} where nombre_emp='{$this->nombre_emp}'";
+        $dato = $this->conexion->QueryResultado($sql);
+        return $dato;
+    }
+
+    function graficar()
+    {
+        $sql="select empleados.id_empleado,puestos.descripcion_puesto as Puesto, COUNT(descripcion_puesto) as Cantidad_Empleados 
+        from empleados, puestos where empleados.id_puesto = puestos.id_puesto GROUP BY descripcion_puesto";
+        $dato = $this->conexion->QueryResultado($sql);
+        return $dato;
+    }
 }
