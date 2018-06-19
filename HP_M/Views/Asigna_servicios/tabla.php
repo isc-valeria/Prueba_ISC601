7f412be6e061b46f7e2eaf6ff35924dcbd108dea
SaulMig <isc_miguel.s@tesvb.edu.mx>
2018-06-19 13:39:00 -0500

    asssigna

--- HP_M/Views/Asigna_servicios/tabla.php
+++ HP_M/Views/Asigna_servicios/tabla.php
@@ -7,7 +7,7 @@ echo "<tr>
 	<td>{$row['descripcion_ser']}</td>
     <td>{$row['descripcion_est']}</td>
     <td>{$row['fecha_reg']}</td>
-            <td><a class=\"btn-flat modal-trigger icon-cross red-text btn_eliminar  \" href='#!' data-id='{$row['id_asignaser']}' ></a></td>
-	<td><a class='btn-flat modal-trigger icon-pencil blue-text' href='#!'></a></td>
+        <td><a class=\"btn-flat modal-trigger icon-cross red-text btn_eliminar\" href='#!' data-id='{$row['id_asignaser']}' ></a></td>
+	    <td><a class='btn-flat modal-trigger icon-pencil blue-text' href='#!'></a></td>
     </tr>";
 //Notice: Undefined index: id_servicios in C:\xampp2\htdocs\Prueba_ISC601\HP_M\Views\Asignacion_de_servicios\tabla.php on line 6
