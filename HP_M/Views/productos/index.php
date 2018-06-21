3ea47e77a866132e7fcb2d9353e1e619141d6013
John Doebernardinooctavio <villegas_3101@hotmail.com>
2018-06-21 18:35:19 -0500

    graf

--- HP_M/Views/productos/index.php
+++ HP_M/Views/productos/index.php
@@ -196,11 +196,21 @@
     </table>
     <div class="center">
         <a href="<?php echo URL?>productos/print_pdf" target="_blank" id="imprimir_pdf" class="btn blue accent-3 white-text tooltipped" data-position="bottom" data-delay="50" data-tooltip="Imprimir"><i class="material-icons">picture_as_pdf</i></a>
-
+        <a href="#!"  id="graficar_productos" class="btn blue accent-3 white-text tooltipped" data-position="bottom" data-delay="50" data-tooltip="Graficar"><i class="material-icons">equalizer</i></a>
     </div>
 </div>
 
+<div id="modal_grafica" class="modal">
+    <div class="modal-content">
+        <h5>Gráfica de productos</h5>
+        <p>
 
+        </p>
+    </div>
+    <div class="modal-footer">
+        <a href="#!" id="cancelar" class="modal-close red white-text waves-effect waves-green btn-flat">Cerrar</a>
+    </div>
+</div>
 
 <div id="modal_eliminar" class="modal">
     <div class="modal-content">
@@ -343,7 +353,13 @@
             $.uiTableFilter($("#tabla_content"), this.value);
         });
 
+        $("#graficar_productos").click(function(){
+            $.get("<?php echo URL?>productos/graficar",function(res){
+                $("#modal_grafica .modal-content p").empty().append(res);
+                $("#modal_grafica").modal("open");
 
+            });
+        });
 
 
     });
