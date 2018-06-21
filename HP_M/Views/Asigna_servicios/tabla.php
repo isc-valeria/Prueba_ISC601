d08f680e02b806e27d37305ca2139c2abcd65471
FelipeEnriqueGG <isc_garcia.f@tesvb.edu.mx>
2018-06-21 11:18:46 -0500

    AsignaServ

--- HP_M/Views/Asigna_servicios/tabla.php
+++ HP_M/Views/Asigna_servicios/tabla.php
@@ -1,13 +1,13 @@
 <?php
-$datos=$datos[0];
+$datos=$datos[ 0];
 while($row=mysqli_fetch_array($datos))
 echo "<tr>
-	<td>{$row['id_asignaser']}</td>
+	<td>{$row[0]}</td>
 	<td>{$row['num_habitacion']}</td>
 	<td>{$row['descripcion_ser']}</td>
     <td>{$row['descripcion_est']}</td>
     <td>{$row['fecha_reg']}</td>
-        <td><a class=\"btn-flat modal-trigger icon-cross red-text btn_eliminar\" href='#!' data-id='{$row['id_asignaser']}' ></a></td>
+        <td><a class='btn-flat modal-trigger icon-cross red-text btn_eliminar' href='#!' data-id='{$row['id_asignaser']}' ></a></td>
 	    <td><a class='btn-flat modal-trigger icon-pencil blue-text' href='#!'></a></td>
     </tr>";
 //Notice: Undefined index: id_servicios in C:\xampp2\htdocs\Prueba_ISC601\HP_M\Views\Asignacion_de_servicios\tabla.php on line 6
