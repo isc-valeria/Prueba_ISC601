c03153e9b171ebb669da05157dd0474f2d7a82ca
Charly2210 <isc_palma.j@tesvb.edu.mx>
2018-07-10 12:32:25 -0500

    salidas

--- HP_M/Views/productos/index.php
+++ HP_M/Views/productos/index.php
@@ -160,7 +160,7 @@
                 <h4 align="center">Registro de Entradas y Salidas <span class="right"><a href="#modal_registro_entrada" class="btn green white-text modal-trigger" id="add_entradas">Entradas
                 <i class="material-icons">add</i>
             </a>
-                        <a href="#modal_registro_salida" class="btn green white-text modal-trigger" id="add_entradas">Salidas
+                        <a href="#modal_registro_salida" class="btn green white-text modal-trigger" id="add_salidas">Salidas
                 <i class="material-icons">add</i>
             </a>
                     </span></h4>
@@ -201,10 +201,7 @@
 
                 </table>
 
-                <div class="center">
-                    <a href="<?php echo URL?>productos/print_pdf" target="_blank" id="imprimir_pdf" class="btn blue accent-3 white-text tooltipped" data-position="bottom" data-delay="50" data-tooltip="Imprimir"><i class="material-icons">picture_as_pdf</i></a>
 
-                </div>
 
 
         </div>
@@ -218,12 +215,12 @@
     <div class="modal-content">
         <div class="card-panel teal #00b8d4"><h4 align="center">Categoria del Producto</h4></div>
         <div class="row">
-            <form class="col s12 ">
+            <form action="" id="save_categoria" class="col s12 ">
                 <div class="row">
                     <div class="input-field col s9">
                         <i class="mdi-action-verified-user prefix icon-pencil"></i>
-                        <input id="categoria_producto" type="text" class="validate">
-                        <label for="categoria_producto"  data-error="incorrecto" data-success="Correcto">Descripcion de Categoria</label>
+                        <input id="descripcion_cat" type="text" name="descripcion_cat">
+                        <label for="descripcion_cat" >Descripcion de Categoria</label>
                     </div>
                 </div>
 
@@ -231,13 +228,11 @@
 
                     <div class="input-field col s12 ">
 
-                        <a href="#!" id="save_productos_ok" class="btn ">Registrar</a>
+                        <a href="#!" id="save_categoria_ok" class="btn ">Registrar</a>
                         <a href="#!" id="cancelar" class="modal-close red white-text waves-effect waves-green btn-flat">Cancelar</a>
 
                     </div>
-                    <div class="input-field col s12">
-                        <a href="#!" id="update_productos_ok" class="btn modal-close " data-id="">Actualizar</a>
-                    </div>
+
 
 
 
@@ -273,6 +268,52 @@
 
                             <input id="cantidad_llegada" type="number" class="validate" >
                             <label for="cantidad_llegada"  data-error="Incorrecto" data-success="Correcto" >Cantidad de llegada</label>
+                        </div>
+                    </div>
+
+
+                    <div class="modal-fixed-footer">
+
+                        <div class="input-field col s12 ">
+
+                            <a href="#!" id="save_productos_ok" class="btn ">Registrar</a>
+                            <a href="#!" id="cancelar" class="modal-close red white-text waves-effect waves-green btn-flat">Cancelar</a>
+
+                        </div>
+
+
+
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <div id="modal_registro_salida" class="modal">
+        <div class="modal-content">
+            <div class="card-panel teal #00b8d4"><h4 align="center">Registro Salida</h4></div>
+            <div class="row">
+                <form class="col s12 ">
+                    <div class="row">
+
+                        <div class="input-field col s8">
+
+                            <input id="nombre_pro" type="text" class="validate">
+                            <label for="nombre_pro"  data-error="incorrecto" data-success="Correcto">Nombre del Producto</label>
+                        </div>
+                    </div>
+
+                    <div class="row ">
+                        <div class="input-field col s5">
+                            <input type="text" class="datepicker" id="fecha_salida">
+                            <label for="fecha_salida" data-error="incorrecto" data-success="Correcto"> Fecha de Llegada</label>
+                        </div>
+
+
+                        <div class="disabled input-field col s4 text ">
+
+                            <input id="cantidad_salida" type="number" class="validate" >
+                            <label for="cantidad_salida"  data-error="Incorrecto" data-success="Correcto" >Cantidad de llegada</label>
                         </div>
                     </div>
 
@@ -452,3 +493,55 @@
 
     });
 </script>
+
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $("#add_categoria").click(function(){
+                $("#save_categoria_ok").show();
+            });
+            $("#save_categoria_ok").click(function(){
+                $.post("<?php echo URL?>productos/crearcategoria",$("#save_tipo").serialize(),function(res){
+                    $("#body_table").empty().append(res);
+                    $('#save_categoria').find('input, select, textarea').val('');
+                    Materialize.updateTextFields();
+                    Materialize.toast('Se ha insertado correctamente', 2500);
+                })
+            });
+        });
+        $("#save_tipo").validate({
+
+            rules:{
+                descripcion_cat:{
+                    required:true,
+                    maxlength: 20,
+                    minlength: 4,
+                    lettersonly:true,
+                },
+
+            },
+            messages:{
+                descripcion_cat:{
+                    required:"Ingresa descripción",
+                    maxlength:"Maximo 20 caracteres",
+                    minlength:"Minimo 4 caracteres",
+
+                },
+
+            },
+            errorPlacement: function(error, element) {
+                $(element)
+                    .closest("form")
+                    .find("label[for='" + element.attr("id") + "']")
+                    .attr('data-error', error.text());
+            },
+            submitHandler:function(form){
+                $.post("<?php echo URL?>Categorias_Producto/crear",$("#save_categoria").serialize(),function(res){
+                    $("#body_table").empty().append(res);
+                    $('#save_categoria').find('input, select, textarea').val('');
+                    Materialize.updateTextFields();
+                    $("#modal_categoria_producto").modal("close");
+                })
+
+            }
+        });
+    </script>
--- HP_M/Views/productos/print_pdf.php
+++ HP_M/Views/productos/print_pdf.php
@@ -5,8 +5,15 @@
  * Date: 14/06/2018
  * Time: 02:52 PM
  */
+
 $pdf = new \AppData\Config\libs\fpdf\FPDF();
 $pdf->AddPage();
+
+$pdf->SetFont('Arial','B',10);
+$pdf->Cell(2);
+$pdf->Image('AppData\Config\libs\fpdf\logo.png','150','3','30','20','PNG','http://localhost/Prueba_ISC601/HP_M/');
+$pdf->Ln();
+
 $pdf->SetFont('Arial','B',16);
 $pdf->Cell(190,10,'Lista de productos.',0,0,'C');
 $pdf->ln();
@@ -18,7 +25,6 @@ $pdf->SetX(25);
 $pdf->Cell(30,10,utf8_decode('nombre'),1,0,'C',true);
 $pdf->Cell(30,10,utf8_decode('categoria'),1,0,'C',true);
 $pdf->Cell(30,10,utf8_decode('Tipo '),1,0,'C',true);
-$pdf->Cell(30,10,utf8_decode('Existencias'),1,0,'C',true);
 $pdf->Cell(30,10,utf8_decode('stock_Max'),1,0,'C',true);
 $pdf->Cell(30,10,utf8_decode('Stock_Min'),1,0,'C',true);
 $pdf->SetFillColor(255,255, 255);
@@ -31,9 +37,9 @@ while($row=mysqli_fetch_row($datos))
     $pdf->Cell(30,8,utf8_decode($row[1]),1,0,'C',true);
     $pdf->Cell(30,8,utf8_decode($row[2]),1,0,'C',true);
     $pdf->Cell(30,8,utf8_decode($row[3]),1,0,'C',true);
-    $pdf->Cell(30,8,utf8_decode($row[4]),1,0,'C',true);
     $pdf->Cell(30,8,utf8_decode($row[5]),1,0,'C',true);
     $pdf->Cell(30,8,utf8_decode($row[6]),1,0,'C',true);
+
 
 
 }
